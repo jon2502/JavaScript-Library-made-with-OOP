cb81f1cdb8005b2eef0de53730b2593513f2e754
jon2502 <jonbjarke25@outlook.dk>
2025-01-15 15:39:17 +0100

    Setup of class constructor

--- Script.js
+++ Script.js
@@ -1,12 +1,35 @@
-var CreateUser = document.getElementById('CreateUser')
+
+var library = document.getElementById('library_Container')
+
+var User = "UserName"
 
 //setup class
-class Book {
+class Books {
+    constructor(title, author, series, cover_image, synopsis, genres, isbn, published_year, isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.series = series;
+        this.cover_image = cover_image;
+        this.synopsis = synopsis;
+        this.genres = genres;
+        this.isbn = isbn;
+        this.published_year = published_year;
+        this.isAvailable = isAvailable;
+    }
 
+    generateBook(){
+        return this.title
+    }
 }
 
-var User = false
 
-async function CreateUser(params) {
-    
+async function createLibrary() {
+    const response = await fetch('Book.json');
+    var BookList = await response.json();
+    console.log(BookList)
+    for(var Book of BookList){
+        var NewBook = new Books(Book.title, Book.author, Book.series, Book.cover_image, Book.synopsis, Book.genres, Book.isbn, Book.published_year, Book.isAvailable)
+        console.log(NewBook.generateBook())
+    }
 }
+createLibrary()
