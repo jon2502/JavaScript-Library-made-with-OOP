8a6c35de44463e8884dead05eda2601ea16fea24
jon2502 <jonbjarke25@outlook.dk>
2025-01-23 13:17:40 +0100

    Added states and state change

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
 var library = document.getElementById('library_Container')
+var Statemanger = document.getElementById('Statemanger')
 
 var User = "UserName"
+var Availbe = true
+var Borrowed = false
 
 //setup class
 class Books {
@@ -15,11 +18,13 @@ class Books {
         this.published_year = published_year;
         this.isAvailable = isAvailable;
     }
+    get(){
 
+    }
     generateBook(){
         const div = document.createElement('div'); // Create a new div element
         //add child content to it
-        div.classList.add('row')
+        div.classList.add('row', 'mb-2')
         div.innerHTML = `
             <div class="col-sm-1">
                 <img class="img-fluid" src = "${this.cover_image}">
@@ -28,7 +33,11 @@ class Books {
                 <h5>${this.title}</h5>
                 <p>By ${this.author}</p>
             </div>
-            <p class="col">${this.series}</p>
+             ${this.series ?`
+                <div class="col">
+                    <p>part of the ${this.series} series</p>
+                </div>
+            `:``}
             <p class="col">ISBN: ${this.isbn}</p>
             <p class="col">${this.published_year}</p>
         `;
@@ -37,6 +46,41 @@ class Books {
     }
 }
 
+async function Checkstate(Availbe, Borrowed) {
+    Statemanger.innerHTML=``
+    switch(true){
+        case Availbe === true:
+            Statemanger.innerHTML=`
+            <h1>avalible books</h1>
+            <button id="changeStat">check borrowed books</button>
+            `
+            var changeEvent = document.getElementById('changeStat')
+            changeEvent.addEventListener('click',function(){
+                Availbe = false
+                Borrowed = true
+                Checkstate(Availbe, Borrowed)
+            })
+        break;
+        case Borrowed === true:
+            Statemanger.innerHTML=`
+            <h1>borrowed books</h1>
+            <button id="changeStat">check avalible books</button>
+            `
+            var changeEvent = document.getElementById('changeStat')
+            changeEvent.addEventListener('click',function(){
+                Availbe = true
+                Borrowed = false
+                Checkstate(Availbe, Borrowed)
+            })
+        break;
+        default:
+            Statemanger.innerHTML =`
+            <h3>An error has ocured</h3>
+            <p> there was an error regading the values. please try again or check the code on line 5 to 6 and.</p>
+            `
+    }
+}
+Checkstate(Availbe, Borrowed)
 
 async function createLibrary() {
     const response = await fetch('Book.json');
@@ -47,4 +91,3 @@ async function createLibrary() {
         library.appendChild(NewBook.generateBook())
     }
 }
-createLibrary()
